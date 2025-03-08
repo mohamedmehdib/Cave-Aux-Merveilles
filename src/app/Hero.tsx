c71dcbdaf28a39a39481dfeb998f5494e9f338f0
Med Mehdi Ben Haj Saleh <medmehdibenhajsaleh1@gmail.com>
2025-03-08 07:01:50 +0100

    first commit

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Hero() {
   return (
     <div className='h-[200vh]'>
-        
+        <div className='pt-52'>
+          <Image src="/test.jpg" alt='test' fill/>
+        </div>
     </div>
   )
 }
