b5cc12532433ed64e80ea8e2327c02caebe7d01a
Med Mehdi Ben Haj Saleh <medmehdibenhajsaleh1@gmail.com>
2025-03-23 12:56:38 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"; // Import Analytics
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
